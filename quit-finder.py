fdf0a976560642ad6353aa3b0becf8e9d110f64d
Sahas Ramesh <sahasramesh22@gmail.com>
2020-06-28 10:14:51 -0700

    fixed labels

--- quit-finder.py
+++ quit-finder.py
@@ -3,8 +3,10 @@ import tkinter as tk
 from tkinter import *
 
 window = Tk()
-window.title("quit finder")
 window.geometry('200x200')
+window.title("quit finder")
+window.rowconfigure(0, weight=1)
+window.columnconfigure(0, weight=1)
 window.resizable(width=False, height=False)
 window.eval('tk::PlaceWindow %s center' % window.winfo_toplevel())
 
@@ -12,10 +14,16 @@ def quit():
     os.system("defaults write com.apple.finder QuitMenuItem -bool true")
     os.system("""osascript -e 'quit app "Finder"'""")
 
-lbl = Label(window, text="let go of your fear.", font=("Times New Roman", 14))
+    lbl.configure(text="was it worth it?")
+    btn.config(state='disabled')
+
+fr = Frame(window)
+fr.grid(column=0, row=0)
+
+lbl = Label(fr, text="let go of your fear.", font=("Times New Roman", 14))
 lbl.grid(column=0, row=0)
 
-btn = Button(window, text="do it.", command=quit, font=("Times New Roman", 14))
+btn = Button(fr, text="do it.", command=quit, font=("Times New Roman", 14))
 btn.grid(column=0, row=1)
 
 window.mainloop()
