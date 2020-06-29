bb6ba104ae3dd4ee725f58bff03294821ebe2ff5
Sahas Ramesh <sahasramesh22@gmail.com>
2020-06-29 10:56:05 -0700

    added tag

--- quit-finder.py
+++ quit-finder.py
@@ -1,4 +1,5 @@
 import os
+import webbrowser
 import tkinter as tk
 from tkinter import *
 
@@ -17,6 +18,9 @@ def quit():
     lbl.configure(text="was it worth it?")
     btn.config(state='disabled')
 
+def tagbtn():
+    webbrowser.open("http://sahasramesh.com")
+
 fr = Frame(window)
 fr.grid(column=0, row=0)
 
@@ -25,5 +29,16 @@ lbl.grid(column=0, row=0)
 
 btn = Button(fr, text="do it.", command=quit, font=("Times New Roman", 14))
 btn.grid(column=0, row=1)
+
+fr1 = Frame(window)
+fr1.grid(column=0, row=2)
+
+#tag
+tag = Label(fr1, text="An original project by ", font=('Courier', 6))
+tag.pack(side=LEFT)
+
+#tag
+btn4 = Button(fr1, text="Sahas Ramesh", fg='#FF4500', anchor=W, highlightbackground='white', highlightcolor='white', highlightthickness=0, command=tagbtn, font=('Courier', 6))
+btn4.pack(side=LEFT)
 
 window.mainloop()
